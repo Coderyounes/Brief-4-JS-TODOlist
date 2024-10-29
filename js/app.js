4b88ad7ed89f63680b2e52ba617d59d47d08bd9b
Coderyounes <younesanas2021@gmail.com>
2024-10-29 21:35:05 +0100

    feat: collecter l'input d'utilisateur directement sans validation & display data sur le console #16

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
 
     // create a function dislay all the Tasks exist in our localStorage
     // function calculate tasks based on their status
     // encapsulate the data inside Object
     // use Data Object
 
-    // show the POPUP Box ( see how to display a div with text & downgrade page opacity)
-    // take the data
-    // validate Data
-    // Display Data
-
     // load the target task
     // take the modifications
     // validate the modification
     // update the modifications
     // display the modification
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // take the data
+    // validate Data
+    // Display Data
     let modal = document.getElementById('popup');
     let createTache = document.getElementById('create');
     let cancel = document.getElementById('cancel');
@@ -32,5 +32,21 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.style.display = "none";
         }
     }
+
+
+
+    let validBtn = document.getElementById('valid');
+    validBtn.onclick = function () {
+        let tacheDetails = {
+            titre: document.getElementById('titre').value,
+            description: document.getElementById('description').value,
+            priorité: document.getElementById('priorite').value,
+            date: document.getElementById('date').value
+        }
+        modal.style.display = "none";
+        
+        console.log(tacheDetails);
+    }
+
 
 });
