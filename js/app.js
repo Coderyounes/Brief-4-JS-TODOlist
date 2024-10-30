82c8abb9f0ef15b04efccef2cc21ef793f1fffb6
Coderyounes <younesanas2021@gmail.com>
2024-10-30 12:03:19 +0100

    feat: ajoute quelque validation  Logic #12

--- js/app.js
+++ js/app.js
@@ -1,31 +1,63 @@
+function ValidateForm(tacheDetails) {
 
-    // create a function dislay all the Tasks exist in our localStorage
-    // function calculate tasks based on their status
-    // encapsulate the data inside Object
-    // use Data Object
+    let isValid = true;
 
-    // load the target task
-    // take the modifications
-    // validate the modification
-    // update the modifications
-    // display the modification
+    const Erros = {
+        titreError: "",
+        descriptionError: "",
+        statusError: "",
+        prioritéError: "",
+        date: "",
+    }
+
+    if (tacheDetails.titre == "" || tacheDetails.titre.length > 5) {
+        Erros.titreError = "Error in titre";
+        document.getElementById('titreError').textContent = Erros.titreError;
+        isValid = false;
+    }
+
+    if (tacheDetails.description == "" || tacheDetails.description.length > 1000) {
+        Erros.descriptionError = "Please Write a Description or make it shorter";
+        document.getElementById('descriptionError').textContent = Erros.descriptionError;
+        isValid = false;
+    }
+
+
+    // need to validate if it select one of options
+    /*if(tacheDetails.status == "") {
+        Erros.statusError = "Wrong Selection";
+        document.getElementById('statusError').textContent = Erros.statusError;
+        isValid = false;
+    }
+
+    // add validation to check if select one of the options
+    if (tacheDetails.priorité = "") {
+        Erros.prioritéError = "Wrong Selection";
+        document.getElementById('prioritéError').textContent = Erros.prioritéError;
+        isValid = false;
+    }
+
+    if (tacheDetails.date = "") {
+        Erros.date = "Wrong date Format";
+        document.getElementById('dateError').textContent = Erros.date;
+        isValid = false;
+    }*/
+    
+    return isValid;
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // take the data
-    // validate Data
-    // Display Data
     let modal = document.getElementById('popup');
-    let createTache = document.getElementById('create');
-    let cancel = document.getElementById('cancel');
 
-    createTache.onclick = function () {
+    document.getElementById('create').onclick = function () {
         modal.style.display = "flex";
     }
 
-    cancel.addEventListener('click', function () {
+    document.getElementById('cancel').onclick = function () {
         modal.style.display = "none";
-    });
+    };
 
     window.onclick = function (e) {
         if (e.target == modal) {
@@ -34,17 +66,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
+    // take the data
+    // validate Data
+    // Display Data
 
-    let validBtn = document.getElementById('valid');
-    validBtn.onclick = function () {
+    document.getElementById('valid').onclick = function () {
+
         let tacheDetails = {
             titre: document.getElementById('titre').value,
             description: document.getElementById('description').value,
             priorité: document.getElementById('priorite').value,
             date: document.getElementById('date').value
+        };
+
+        let isValid = ValidateForm(tacheDetails);
+
+        if (isValid) {
+            // trigger to add function
         }
-        modal.style.display = "none";
-        
+
+        //clearinputs
+        //modal.style.display = "none";
+
         console.log(tacheDetails);
     }
 
